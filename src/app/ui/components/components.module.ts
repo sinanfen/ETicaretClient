5042e10e97f2ad6ad3dc2ec366205e7eab97ead0
Sinan <sinanfen@hotmail.com>
2023-04-28 03:46:16 +0300

    updated

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -5,19 +5,22 @@ import { HomeModule } from './home/home.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { RegisterComponent } from './register/register.component';
 import { RegisterModule } from './register/register.module';
+import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
 
 
 
 @NgModule({
   declarations: [
-    
+
   ],
   imports: [
     CommonModule,
     ProductsModule,
     HomeModule,
     BasketsModule,
-    RegisterModule
+    RegisterModule,
+    LoginModule
   ]
 })
 export class ComponentsModule { }
