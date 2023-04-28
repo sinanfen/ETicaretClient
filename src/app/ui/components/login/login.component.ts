83347524c7819b521b771c9df83a7f2d45ea032b
Sinan <sinanfen@hotmail.com>
2023-04-28 23:51:25 +0300

    login operation completed with username or email

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService, spinner: NgxSpinnerService) {
+    super(spinner)
+  }
 
   ngOnInit(): void {
   }
 
+
+  async login(usernameOrEmail: string, password: string) {
+    this.showSpinner(SpinnerType.BallAtom);
+    await this.userService.login(usernameOrEmail, password, () => this.hideSpinner(SpinnerType.BallAtom));
+  }
 }
